eca74fcf5732c0ba2de202eb7be92147cb2ebaec
Gabriel Lima <gabriel.lima@imogen.com.br>
2025-09-15 13:28:19 -0300

    Add code to session entry

--- tools/messages/session_entry_created_message.py
+++ tools/messages/session_entry_created_message.py
@@ -2,6 +2,34 @@ from easter import Message
 
 
 class SessionEntryCreatedMessage(Message):
-    def __init__(self, target_id: str, units: list[str]):
-        self.target_id = target_id
+    def __init__(self, code: str, target: str, units: list[str]):
+        self.code = code
+        self.target = target
         self.units = units
+
+    class Builder:
+        def __init__(self):
+            self.code = None
+            self.target = None
+            self.units = []
+
+        def with_code(self, code: str):
+            self.code = code
+            return self
+
+        def with_target(self, target: str):
+            self.target = target
+            return self
+
+        def with_units(self, units: list[str]):
+            self.units = units
+            return self
+
+        def add_unit(self, unit: str):
+            self.units.append(unit)
+            return self
+
+        def build(self):
+            if self.code is None or self.target is None:
+                raise ValueError("Code and target must be set")
+            return SessionEntryCreatedMessage(self.code, self.target, self.units)

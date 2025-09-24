d608ba332893b1e03bdfc57e18d225965cb0a68f
Gabriel Lima <gabriel.lima@imogen.com.br>
2025-09-24 13:18:35 -0300

    Add easier import

--- tools/messages/__init__.py
+++ tools/messages/__init__.py
@@ -1,1 +1,4 @@
 from tools.messages.session_entry_created_message import SessionEntryCreatedMessage
+from tools.messages.session_engine_records_created_message import (
+    SessionEngineRecordsCreatedMessage,
+)

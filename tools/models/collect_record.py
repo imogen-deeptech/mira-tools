41cf0cd985ecc3c598357a2bc4ac56a9203c9212
Gabriel Lima <gabriel.lima@imogen.com.br>
2025-12-10 22:34:33 -0300

    Add chassis status to collect record

--- tools/models/collect_record.py
+++ tools/models/collect_record.py
@@ -8,11 +8,13 @@ class CollectRecord(object):
         target: str,
         order: int,
         chassis: str,
+        chassis_status: Optional[str] = None,
         engine: Optional[str] = None,
     ):
         self.code = code
         self.target = target
         self.order = order
         self.chassis = chassis
+        self.chassis_status = chassis_status
         self.engine = engine
 
